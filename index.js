5c24d3ec1915a6b6535305da350fc727c85280dc
Vivek Kumar Verma(IN3119) <M1043119@mindtree.com>
2018-07-25 11:24:25 +0530

    webRTC added

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var app = express();
 
+var socketIO = require('socket.io');
+
+var server = require('http').createServer(app);
+
 var solution = [{"id":"mongo",
 "value":"After the analysis, I would suggest that, Mongo DB is better suited for your project!"
 },{"id":"sql",
@@ -285,9 +289,67 @@ app.use(function(req,res,next){
   });
 
 
-app.get('/botroute', function(req, res){
-  res.send("You are at right place, let me help you !");
-});
+  var io = socketIO.listen(server);
+  io.sockets.on('connection', function(socket) {
+  
+    // convenience function to log server messages on the client
+    function log() {
+      var array = ['Message from server:'];
+      array.push.apply(array, arguments);
+      socket.emit('log', array);
+    }
+  
+    socket.on('message', function(message) {
+      log('Client said: ', message);
+      // for a real app, would be room-only (not broadcast)
+      socket.broadcast.emit('message', message);
+    });
+  
+    socket.on('create or join', function(room) {
+      log('Received request to create or join room ' + room);
+  
+      var clientsInRoom = io.sockets.adapter.rooms[room];
+      var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+      log('Room ' + room + ' now has ' + numClients + ' client(s)');
+  
+      if (numClients === 0) {
+        socket.join(room);
+        log('Client ID ' + socket.id + ' created room ' + room);
+        socket.emit('created', room, socket.id);
+  
+      } else if (numClients === 1) {
+        log('Client ID ' + socket.id + ' joined room ' + room);
+        io.sockets.in(room).emit('join', room);
+        socket.join(room);
+        socket.emit('joined', room, socket.id);
+        io.sockets.in(room).emit('ready');
+      } else { // max two clients
+        socket.emit('full', room);
+      }
+    });
+  
+    socket.on('ipaddr', function() {
+      var ifaces = os.networkInterfaces();
+      for (var dev in ifaces) {
+        ifaces[dev].forEach(function(details) {
+          if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+            socket.emit('ipaddr', details.address);
+          }
+        });
+      }
+    });
+  
+    socket.on('bye', function(){
+      console.log('received bye');
+    });
+  
+  });
+  
+
+
+// app.get('/botroute', function(req, res){
+//   res.send("You are at right place, let me help you !");
+// });
 
 app.post('/dataSolution',function(req,res){
     // console.log(req.body)
@@ -337,6 +399,6 @@ app.post('/botroute',function(req,res){
     }
 })
 
-app.listen(4000, function(){
+server.listen(4000, function(){
     console.log("Server Running on port 4000....")
 });
